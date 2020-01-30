9a6b32b542f95eb9b3133e1692b146157fac0b89
connorgaskell <48499463+connorgaskell@users.noreply.github.com>
2020-01-30 19:49:33 +0000

    Implemented Diffuse Shader for object/enemy highlighting.

---
+++ Assets/Scripts/Objects/Highlight.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Highlight : MonoBehaviour {
+
+    public Renderer rend;
+
+    void OnMouseOver() {
+        rend.material.shader = Shader.Find("Outlined/Diffuse");
+    }
+
+    void OnMouseExit() {
+        rend.material.shader = Shader.Find("Diffuse");
+    }
+    
+}
